f315b0336eee827bd4f3510326859ed301f60831
Gauthier Masse <masse.gauthier@gmail.com>
2018-04-19 22:56:11 +0200

    avec fin de partie

--- morbac.rb
+++ morbac.rb
@@ -18,6 +18,7 @@ class Board
         @plateau[y][:"#{x}"] = Boardcase.new
       end
     end
+    @fin = false
   end
 
   def puts_plateau
@@ -38,22 +39,32 @@ class Board
   	#line+column 	
  	for i in 0..2
  		if (plateau[i][:"0"].value == plateau[i][:"1"].value && plateau[i][:"0"].value == plateau[i][:"2"].value) && (plateau[i][:"0"].value != "")			
- 			puts "le joueur ayant le symbole #{plateau[i][:"0"].value} gagne avec la ligne #{i}"	
+ 			puts "FIN DE LA PARTIE! Le joueur jouant avec le symbole #{plateau[i][:"0"].value} gagne avec la ligne #{i}."
+ 			@fin = true	
  		elsif (plateau[0][:"#{i}"].value == plateau[1][:"#{i}"].value && plateau[0][:"#{i}"].value == plateau[2][:"#{i}"].value) && (plateau[0][:"#{i}"].value != "")
- 			puts "le joueur ayant le symbole #{plateau[0][:"#{i}"].value} gagne avec la colonne #{i}"
+ 			puts "FIN DE LA PARTIE! Le joueur jouant avec le symbole #{plateau[0][:"#{i}"].value} gagne avec la colonne #{i}."
+ 			@fin = true
  		else
 		end 
 	end
 
 	#diago
-		if (plateau[0][:"0"].value == plateau[1][:"1"].value && plateau[0][:"0"].value == plateau[2][:"2"].value) && (plateau[0][:"0"].value != "")		
- 			puts "le joueur ayant le symbole #{plateau[0][:"0"].value} gagne avec la première diagonale"	
- 		elsif (plateau[0][:"2"].value == plateau[1][:"1"].value && plateau[0][:"2"].value == plateau[2][:"0"].value) && (plateau[0][:"2"].value != "")		
- 			puts "le joueur ayant le symbole #{plateau[0][:"2"].value} gagne avec la diagonale inversée"
- 		else
- 		end
- 	""
+	if (plateau[0][:"0"].value == plateau[1][:"1"].value && plateau[0][:"0"].value == plateau[2][:"2"].value) && (plateau[0][:"0"].value != "")		
+ 		puts "FIN DE LA PARTIE! Le joueur jouant avec le symbole #{plateau[0][:"0"].value} gagne avec la première diagonale."	
+ 		@fin = true
+ 	elsif (plateau[0][:"2"].value == plateau[1][:"1"].value && plateau[0][:"2"].value == plateau[2][:"0"].value) && (plateau[0][:"2"].value != "")		
+ 		puts "FIN DE LA PARTIE! Le joueur jouant avec le symbole #{plateau[0][:"2"].value} gagne avec la diagonale inversée."
+ 		@fin = true
+	else
+
+   	end
+   	
+   	if @fin == false
+   		return ""
+   	else
+   		return "nacre"
  	end
+ end
 
 end
  
@@ -72,7 +83,7 @@ class Game
   end
 
   def next_turn
-  	puts "#{@player[@turn%2].name}, où veux tu mettre ton pion? un chiffre pour la ligne (0 à 2), le second pour la colonne (0 à 2)"
+  	puts "A toi,#{@player[@turn%2].name}! Où veux tu mettre ton pion? Indique un chiffre entre 0 et 2 pour la ligne, puis un second entre 0 et 2 pour la colonne."
   	@ligne = gets.chomp.to_i
   	@colonne = gets.chomp.to_i
   		if @turn%2 == 0
@@ -85,13 +96,15 @@ class Game
   end
 
   def runprogram
-	puts "bienvenue, morpionneurs"
+	puts "Bienvenue, morpionneurs. Que la force soit avec vous."
 	board.puts_plateau
-	while board.verif == "" do
+	while board.verif == ""
 		next_turn
 	end
-	coucou.board.verif
-  end
+
+	puts "Merci d'avoir joué, on recommence?"
+	
+end
 
 end #end de class game
 
@@ -100,7 +113,7 @@ end #end de class game
 class Player
   attr_accessor :name, :symbol
   def initialize(symbol)
-  	puts "Nom de joueur"
+  	puts "Prénom du joueur?"
     @name = gets.chomp.to_s
     @symbol = symbol
   end
